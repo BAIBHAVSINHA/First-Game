7497e5e4fc0ce5fc3231389ea9e7f9448074eed3
Baibhav Sinha <60212736+BAIBHAVSINHA@users.noreply.github.com>
2020-09-02 21:30:09 +0530

    Update and rename edit method to edit method.c

---
+++ edit method.c
@@ -0,0 +1,25 @@
+// On edit, add scroll event handler, and display combobox
+protected override void Edit(System.Windows.Forms.CurrencyManager 
+    source, int rowNum, System.Drawing.Rectangle bounds, bool readOnly, 
+    string instantText, bool cellIsVisible)
+{
+    //Call base method which is important else 
+    //edit will not function properly
+    base.Edit(source, rowNum, bounds, readOnly, 
+                     instantText, cellIsVisible);
+// Check if cell is not empty
+    if(this.TextBox.Text.TrimEnd()!="")
+    {
+        // Get Column position
+
+        int dataValue = Int32.Parse(this.TextBox.Text);
+        int pos = this.MappingName.LastIndexOf("col");
+        if(pos > -1)
+        {
+            string colIndex = this.MappingName.Substring(pos+3);
+            int colPos = Int32.Parse(colIndex);
+            // Check whether it is answer spot.
+            _answerPostion=_game.CheckIfAnswerPosition(rowNum,
+                                              colPos,dataValue);
+        }
+    }
